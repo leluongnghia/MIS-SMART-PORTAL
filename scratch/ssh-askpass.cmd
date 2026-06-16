c5001ffa438cc5587f96ee64de2230a4e4ef592c
hoangduongmba-star <hoangduongmba-star@users.noreply.github.com>
2026-06-16 14:57:27 +0700

    chore: update .gitignore and remove cached files (db backup, scratch, cjs)

--- scratch/ssh-askpass.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-echo d123456
--- seed_leads.cjs
+++
@@ -1,6 +0,0 @@
-
-const { PGlite } = require("@electric-sql/pglite");
-const { drizzle } = require("drizzle-orm/pglite");
-const { v4: uuidv4 } = require("uuid");
-const schema = require("./src/models/Schema.ts"); // this is ts, we cant require it directly in cjs.
-
